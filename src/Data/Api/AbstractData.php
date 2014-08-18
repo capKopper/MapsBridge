52828dd373acab3f7b82d85c518d0261a7f94460
Bprod <bifprod@gmail.com>
2014-08-18 16:13:06 +0200

    Add a deserialization base method that mimics the default JMS behavior, to be extended by child classes.

--- src/Data/Api/AbstractData.php
+++ src/Data/Api/AbstractData.php
@@ -15,6 +15,8 @@
 namespace MapsSystem\Bridge\Data\Api;
 
 use JMS\Serializer\Annotation AS JMS;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\JsonDeserializationVisitor;
 
 /**
  * Base for building any kind of MaPS System® API data.
@@ -73,7 +75,43 @@ abstract class AbstractData implements DataInterface
      */
     public function isValid()
     {
-        return STATUS_CUSTOM;
+        // The API should never return a status of 0, but always positive integers.
+        return (int) $this->getResponseStatus() > 0;
+    }
+
+    /**
+     * Provide a base for a JSON deserialization callback that perform the same mapping
+     * using annotations than default JMS class.
+     *
+     * The purpose of this method is to allow child classes to perform basic mapping as
+     * usual, but also perform custom handling of non-exposed properties.
+     *
+     * @return \MapsSystem\Bridge\Data\Api\DataInterface
+     */
+    public function deserializeFromJson(JsonDeserializationVisitor $visitor, $data, DeserializationContext $context)
+    {
+        // Use the standard annotation mechanism.
+        $type = array('name' => __CLASS__, 'params' => array());
+        $metadata = $context->getMetadataFactory()->getMetadataForClass(__CLASS__);
+        $exclusionStrategy = $context->getExclusionStrategy();
+        $visitor->startVisitingObject($metadata, $this, $type, $context);
+
+        foreach ($metadata->propertyMetadata as $propertyMetadata) {
+            if (null !== $exclusionStrategy && $exclusionStrategy->shouldSkipProperty($propertyMetadata, $context)) {
+                continue;
+            }
+
+            if ($context instanceof DeserializationContext && $propertyMetadata->readOnly) {
+                continue;
+            }
+
+            $context->pushPropertyMetadata($propertyMetadata);
+            $visitor->visitProperty($propertyMetadata, $data, $context);
+            $context->popPropertyMetadata();
+        }
+
+        $visitor->endVisitingObject($metadata, $data, $type, $context);
+        return $this;
     }
 
 }
